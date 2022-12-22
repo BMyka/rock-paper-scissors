beb7a48a848ecf30177b1bb318f6e150b158fe46
myka <98901420+BMyka@users.noreply.github.com>
2022-12-22 23:35:21 +0200

    Update, Clickable buttons

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-let computerScore;
-let userScore;
+let computerScore = 0;
+let userScore = 0;
+let buttonsAdded = false;
 
 const winningCombinations = [
     [2, 1], // paper beats rock
@@ -10,10 +11,10 @@ const winningCombinations = [
 
 function playRound(computerGenerated, computerPick, userPick) {
   const userPickText = ["Rock", "Paper", "Scissors"][userPick - 1];
-
+  
   switch (true) {
     case computerGenerated === userPick:
-      return `It was a draw!`;
+      return `We picked the same thing!`;
     case winningCombinations.some(([cg, up]) => cg === computerGenerated && up === userPick):
       computerScore++;
       return `I won, ${computerPick} beats ${userPickText}!`;
@@ -23,42 +24,112 @@ function playRound(computerGenerated, computerPick, userPick) {
   }
 }
 
-function game() {
-  while (true) {
-    computerScore = 0;
-    userScore = 0;
-
-    for (let i = 1; i <= 5; i++) {
-
-
-      // Computer chooses
+let counter = 0;
+function game(e) {
+      counter++;
+              // Computer chooses
       const computerGenerated = Math.floor(Math.random() * 3) + 1;
       const computerPick = ["Rock", "Paper", "Scissors"][computerGenerated - 1];
 
       // Users chooses his pick
+      const pick = parseInt(e.target.getAttribute('data-pick'));
 
-      const userInput = prompt("Enter your pick, Rock/Paper/Scissors").toLowerCase();
-      const userPick = ["rock", "paper", "scissors"].indexOf(userInput) + 1;
-
-      alert(playRound(computerGenerated, computerPick, userPick));
-    }
-
+      const result = document.querySelector('.result');
+      result.textContent = playRound(computerGenerated, computerPick, pick)
+      
+      
+    const score = document.querySelector('.score');
     if (computerScore === userScore) {
-      alert(`It was a draw: ${userScore} - ${computerScore}`);
+        score.textContent = `It was a draw: ${userScore} - ${computerScore}`;
     } else if (computerScore > userScore) {
-      alert(`You lost: ${userScore} - ${computerScore}`);
+        score.textContent = `Score: ${userScore} - ${computerScore}`;
     } else {
-      alert(`You won: ${userScore} - ${computerScore}`);
+        score.textContent = `Score: ${userScore} - ${computerScore}`;
     }
 
-    // ask the user if they want to play again
-    const playAgain = prompt("Do you want to play again? (yes/no)").toLowerCase();
-    if (playAgain !== "yes") {
-      break; // exit the loop
-    }
-  }
+
+    if(counter === 5){
+      hideSquares();
+
+      const container = document.querySelector('#container');
+      const paragraph = document.createElement('p');
+      const tryAgain = document.createElement('div');
+      tryAgain.classList.add('question');
+      tryAgain.textContent = "Do you want to play again?";
+      let answer = ""
+      const yes = document.createElement('button');
+      const no = document.createElement('button');
+      yes.classList.add('yes');
+      no.classList.add('no');
+      yes.textContent = "Yes";
+      no.textContent = "No";
+      tryAgain.append(yes, no);
+      container.append(tryAgain);
+      
+      const YES = document.querySelector('.yes');
+      const NO = document.querySelector('.no');
+    
+      YES.addEventListener('click', function(e) {
+        answer = e.target.textContent;
+        console.log(answer);
+      });
+    
+      NO.addEventListener('click', function(e) {
+        answer = e.target.textContent;
+        console.log(answer);
+      });
+
+      return new Promise((resolve, reject) => {
+        const checkAnswer = setInterval(() => {
+          if (answer !== "") {
+            clearInterval(checkAnswer);
+            if (answer == "Yes") {
+              container.removeChild(tryAgain);
+              showSquares();
+              score.textContent = "";
+              result.textContent = "";
+              computerScore = 0;
+              userScore = 0;
+              counter = 0;
+              const header = document.querySelector('h1');
+              header.textContent = "Computer vs Human... Who will win"
+              resolve();
+            }
+            if (answer == "No") {
+              container.removeChild(tryAgain);
+              score.textContent = "Ok, have it your way";
+              result.textContent = "";
+              computerScore = 0;
+              userScore = 0;
+              counter = 0;
+              const header = document.querySelector('h1');
+              header.textContent = ""
+              resolve();
+            }
+          }
+        }, 100);
+      });
+
+    }  
+
 }
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => square.addEventListener('click', game))
 
-game();
 
+
+const hideSquares = () => {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.style.display = 'none';
+  });
+};
+
+
+const showSquares = () => {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.style.display = 'block';
+  });
+};
 
